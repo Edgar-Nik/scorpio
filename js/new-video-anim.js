8399749809aff04eb825d828018f9ade378e0de7
Edgar <edgarnikogosian89@gmail.com>
2020-12-06 15:24:47 +0400

    video anim changed

---
+++ js/new-video-anim.js
@@ -0,0 +1,61 @@
+let video = document.getElementById("myVideo");
+let textAnim = 'Most consumers are not ready to purchase what you offer';
+let container = document.querySelector('.top-container')
+let textAnim2 = document.querySelector('.animation-text-second')
+let textAnim3 = document.querySelector('.animation-text-opacity')
+
+let changeField = document.getElementById('typing-text')
+
+function startVideo(parent,child){
+    video.play()
+    parent.removeChild(child)
+}
+
+function delayInner(field,secondField,lastField,i, value) {
+    setTimeout(() => {
+        field.innerHTML += value;
+        if(field.innerHTML===textAnim){
+            secondField.classList.add('animation-text-second-animate') 
+            lastField.classList.add('animation-text-opacity-animate')
+        }
+    }, i * 100);
+    
+  }
+  
+  function homeAnim(field,secondField,lastField) {
+     
+        for (let i = 0; i < textAnim.length; i++) {
+            delayInner(field,secondField,lastField,i, textAnim[i]);    
+        
+      }
+  }
+
+
+video.addEventListener("ended", () => {
+ 
+  console.log('ended')
+  let newElem = document.createElement('div')
+  let newField = document.createElement('p')
+  newField.classList.add('animation-change-text')
+  
+  newElem.classList.add('showVideo_text')
+  
+  container.appendChild(newElem)
+  newElem.appendChild(newField)
+
+  let secondField = document.createElement('p')
+  newElem.appendChild(secondField)
+  secondField.classList.add('animation-text-second')
+  secondField.innerHTML='YET';
+  let lastField = document.createElement('p')
+  lastField.classList.add('animation-text-opacity')
+  newElem.appendChild(lastField)
+  lastField.innerHTML='We are here to change that reality'
+  homeAnim(newField,secondField,lastField)
+ 
+  setTimeout(()=>{
+
+    startVideo(container,newElem)
+  },20000)
+ 
+});
