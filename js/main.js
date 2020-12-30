3c87e89334690eb75a9f3a96e1b79cee9bc44598
Edgar <edgarnikogosian89@gmail.com>
2020-12-30 17:27:46 +0400

    changed texts

--- js/main.js
+++ js/main.js
@@ -12,6 +12,56 @@ if (document.querySelector(".menu-icon-wrapper")) {
 
     document.querySelector(".menu-icon").classList.toggle("menu-icon-active");
     document.querySelector(".header").classList.toggle("menu-icon-active");
+    if(document.body.classList.contains("active-menu")){
+
+      setTimeout(()=>{
+        let textWrapper = document.querySelector('.ml12');
+        textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        
+        anime.timeline({loop: false})
+          .add({
+            targets: '.ml12 .letter',
+            translateX: [40,0],
+            translateZ: 0,
+            opacity: [0,1],
+            easing: "easeOutExpo",
+            duration: 3200,
+            delay: (el, i) => 500 + 30 * i
+          })
+        
+        let textWrapper1 = document.querySelector('.ml12_1');
+        textWrapper1.innerHTML = textWrapper1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        
+        anime.timeline({loop: false})
+          .add({
+            targets: '.ml12_1 .letter',
+            translateX: [40,0],
+            translateZ: 0,
+            opacity: [0,1],
+            easing: "easeOutExpo",
+            duration: 3200,
+            delay: (el, i) => 500 + 30 * i
+          })
+        
+        
+        
+          let textWrapper2 = document.querySelector('.ml12_2');
+        textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        
+        anime.timeline({loop: false})
+          .add({
+            targets: '.ml12_2 .letter',
+            translateX: [40,0],
+            translateZ: 0,
+            opacity: [0,1],
+            easing: "easeOutExpo",
+            duration: 3200,
+            delay: (el, i) => 500 + 30 * i
+          })
+      },500)
+    
+
+    }
   };
 }
 
@@ -36,3 +86,6 @@ if (document.body.clientWidth < 500) {
     }
   });
 }
+
+
+// Wrap every letter in a span
--- js/new-video-anim.js
+++ js/new-video-anim.js
@@ -6,11 +6,16 @@ let textAnim3 = document.querySelector('.animation-text-opacity')
 
 let changeField = document.getElementById('typing-text')
 
-function startVideo(parent,child){
-    video.play()
-    parent.removeChild(child)
-}
+// function startVideo(parent,child){
+//     video.play()
+//     parent.removeChild(child)
+// }
+function startVideo(){
+  document.querySelector('.animated-texts-wrap').style.display='none'
 
+  video.play()
+
+}
 function delayInner(field,secondField,lastField,i, value) {
     setTimeout(() => {
         field.innerHTML += value;
@@ -33,28 +38,102 @@ function delayInner(field,secondField,lastField,i, value) {
 
 video.addEventListener("ended", () => {
  
-  console.log('ended')
-  let newElem = document.createElement('div')
-  let newField = document.createElement('p')
-  newField.classList.add('animation-change-text')
+  // console.log('ended')
+  // let newElem = document.createElement('div')
+  // let newField = document.createElement('p')
+  // newField.classList.add('animation-change-text')
   
-  newElem.classList.add('showVideo_text')
-  container.appendChild(newElem)
-  newElem.appendChild(newField)
+  // newElem.classList.add('showVideo_text')
+  // container.appendChild(newElem)
+  // newElem.appendChild(newField)
 
-  let secondField = document.createElement('p')
-  newElem.appendChild(secondField)
-  secondField.classList.add('animation-text-second')
-  secondField.innerHTML='YET';
-  let lastField = document.createElement('p')
-  lastField.classList.add('animation-text-opacity')
-  newElem.appendChild(lastField)
-  lastField.innerHTML='WE ARE HERE TO CHANGE THAT REALITY'
-  homeAnim(newField,secondField,lastField)
+  // let secondField = document.createElement('p')
+  // newElem.appendChild(secondField)
+  // secondField.classList.add('animation-text-second')
+  // secondField.innerHTML='YET';
+  // let lastField = document.createElement('p')
+  // lastField.classList.add('animation-text-opacity')
+  // newElem.appendChild(lastField)
+  // lastField.innerHTML='WE ARE HERE TO CHANGE THAT REALITY'
+  // homeAnim(newField,secondField,lastField)
+  document.querySelector('.animated-texts-wrap').style.display='flex'
+
+  let textWrapper = document.querySelector('.ml13');
+  let textWrapper1 = document.querySelector('.ml13_1');
+
+  let textWrapper2 = document.querySelector('.ml13_2');
+
+  if(textWrapper && textWrapper1 && textWrapper2){
+    textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+  
+    anime.timeline({loop: false})
+      .add({
+        targets: '.ml13 .letter',
+        translateY: [100,0],
+        translateZ: 0,
+        opacity: [0,1],
+        easing: "easeOutExpo",
+        duration: 1400,
+        delay: (el, i) => 300 + 30 * i
+      }).add({
+        targets: '.ml13 .letter',
+        translateY: [0,-100],
+        opacity: [1,0],
+        easing: "easeInExpo",
+        duration: 1200,
+        delay: (el, i) => 100 + 30 * i
+      });
+  
+      textWrapper1.innerHTML = textWrapper1.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+      
+      anime.timeline({loop: false})
+        .add({
+          targets: '.ml13_1 .letter',
+          translateY: [100,0],
+          translateZ: 0,
+          opacity: [0,1],
+          easing: "easeOutExpo",
+          duration: 1400,
+          delay: (el, i) => 300 + 30 * i
+        }).add({
+          targets: '.ml13_1 .letter',
+          translateY: [0,-100],
+          opacity: [1,0],
+          easing: "easeInExpo",
+          duration: 1200,
+          delay: (el, i) => 100 + 30 * i
+        });
+        textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+        
+        anime.timeline({loop: false})
+          .add({
+            targets: '.ml13_2 .letter',
+            translateY: [100,0],
+            translateZ: 0,
+            opacity: [0,1],
+            easing: "easeOutExpo",
+            duration: 1400,
+            delay: (el, i) => 300 + 30 * i
+          }).add({
+            targets: '.ml13_2 .letter',
+            translateY: [0,-100],
+            opacity: [1,0],
+            easing: "easeInExpo",
+            duration: 1200,
+            delay: (el, i) => 100 + 30 * i
+          });
+  
+  }
  
+  // setTimeout(()=>{
+
+  //   startVideo(container,newElem)
+  // },10000)
   setTimeout(()=>{
 
-    startVideo(container,newElem)
-  },10000)
+    startVideo()
+  },8000)
+
+
  
 });
